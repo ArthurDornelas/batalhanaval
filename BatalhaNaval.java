dc272354c15c609b3556257b8a48d8041ae868f5
ArthurDornelas <arthurdornelas99@gmail.com>
2018-08-20 17:53:04 -0300

    Create BatalhaNaval.java

---
+++ BatalhaNaval.java
@@ -0,0 +1,14 @@
+package batalhanaval;
+import java.util.Scanner;
+
+public class BatalhaNaval {
+
+   
+    public static void main(String[] args) {
+        
+        Jogo j1 = new Jogo();
+        j1.jogadores();
+        j1.checaPalpite();
+        
+    }
+}
