52a08c8283e0167e4be3fd2f31f8f6e850435162
ArthurDornelas <arthurdornelas99@gmail.com>
2018-08-20 17:54:19 -0300

    Create Jogador.java

---
+++ Jogador.java
@@ -0,0 +1,28 @@
+
+
+package batalhanaval;
+
+/**
+ *
+ * @author ice
+ */
+public class Jogador {
+    
+    //public String nome;
+    //public int idade;
+    public int palpite;
+    public int num;
+    
+    public Jogador(int num) {
+        //this.nome = nome;
+        //this.idade = idade;
+        this.num = num;
+    }
+    
+    public void Palpite(int p){
+        this.palpite = p;
+        
+    }
+    
+    
+}
